f2e75bfefcfa6a31cbae21f80229239644b0773d
korxsong <korxsong@gmail.com>
2019-04-08 23:13:10 +0900

    0.04

--- joker.js
+++ joker.js
@@ -38,6 +38,8 @@ var NGuildBattleDMG = document.querySelector("#NGuildBattleDMG");
 var ArenaDMG = document.querySelector("#ArenaDMG");
 // 19. 치명타 미발생시 아레나 데미지
 var NArenaDMG = document.querySelector("#NArenaDMG");
+// 20. 크리티컬 확률
+var CRIRATE = document.querySelector("#CRIRATE");
 
 function cal() {
     if (
@@ -53,12 +55,12 @@ function cal() {
             isNaN(LCRIDMG.value),
             isNaN(IAP.value),
             isNaN(SKILLLV.value),
-            isNaN(FIGHTRUNE.value))
+            isNaN(FIGHTRUNE.value),
+            isNaN(CRIRATE.value)
+        )
     ) {
         alert("입력칸 내에는 숫자만 입력해주세요.");
     }
-
-
 
     if (ATK.value == "") {
         ATK.value = "0";
@@ -67,32 +69,29 @@ function cal() {
         CRI.value = "0";
     }
     if (LATK.value == "") {
-        LATK.value = "0"
+        LATK.value = "0";
     }
     if (LCRIDMG.value == "") {
-        LCRIDMG.value = "0"
+        LCRIDMG.value = "0";
     }
     if (FIGHTRUNE.value == "") {
-        FIGHTRUNE.value = "0"
+        FIGHTRUNE.value = "0";
     }
-
-
-    // if (
-    //     ATK.value == "0"
-    // ) {
-    //     alert("공격력을 입력해주세요.");
-    // }
-
-    // if (
-    //     CRI.value == "0"
-    // ) {
-    //     alert("치명 피해를 입력해주세요.");
-    // }
-
-    // a= 기본공격력 (((a+(a*고대의검)+(a*바람의성소))*공업유무)*퍼뎀)*(1+(((치피+치피건물)*0.01)+스킬레벨))
+    if (CRIRATE.value == "") {
+        CRIRATE.value = "0";
+    }
+    if (CRIRATE.value > 100) {
+        CRIRATE.value = "100";
+    }
+    ATK.value = parseInt(ATK.value, 10);
+    CRI.value = parseInt(CRI.value, 10);
+    LATK.value = parseInt(LATK.value, 10);
+    LCRIDMG.value = parseInt(LCRIDMG.value, 10);
+    FIGHTRUNE.value = parseInt(FIGHTRUNE.value, 10);
+    CRIRATE.value = parseInt(CRIRATE.value, 10);
 
     //------------------------------------------------------------------------------------------------------------------------------------//
-    //카이로스던전
+    //카이로스던전(기댓값)
     var Cairos;
     Cairos =
         // BASE ATK + RUNE ATK
@@ -103,7 +102,7 @@ function cal() {
             ((eval(AS.value) +
                     eval(WS.value) +
                     eval(LATK.value) +
-                    (eval(FIGHTRUNE.value) * 8) +
+                    eval(FIGHTRUNE.value) * 8 +
                     eval(GB.value)) *
                 0.01)) *
         // INCREASE ATK BUFF
@@ -111,10 +110,44 @@ function cal() {
         0.68 *
         // PLUS CIRDMG
         (1 +
-            ((eval(CRI.value) + eval(FAG.value) + eval(LCRIDMG.value)) * 0.01 +
+            (((eval(CRI.value) + eval(FAG.value) + eval(LCRIDMG.value) + eval(GB.value)) * 0.01) * eval(CRIRATE.value) * 0.01 +
                 eval(SKILLLV.value))) *
-        0.877;
+        (1000 / 1140);
     CairosDMG.innerHTML = Math.ceil(Cairos);
+    MAXCairosDMG.innerHTML = Math.ceil(Cairos * 1.03);
+    MINCairosDMG.innerHTML = Math.ceil(Cairos * 0.97);
+    MAXallCairosDMG.innerHTML = Math.ceil(Cairos * 3 * 1.03);
+    MINallCairosDMG.innerHTML = Math.ceil(Cairos * 3 * 0.97);
+
+    //------------------------------------------------------------------------------------------------------------------------------------//
+    //카이로스던전(전부 치명타)
+    var Cairos;
+    Cairos =
+        // BASE ATK + RUNE ATK
+        (900 +
+            eval(ATK.value) +
+            // ARENA and READER ATK
+            900 *
+            ((eval(AS.value) +
+                    eval(WS.value) +
+                    eval(LATK.value) +
+                    eval(FIGHTRUNE.value) * 8 +
+                    eval(GB.value)) *
+                0.01)) *
+        // INCREASE ATK BUFF
+        eval(IAP.value) *
+        0.68 *
+        // PLUS CIRDMG
+        (1 +
+            (((eval(CRI.value) + eval(FAG.value) + eval(LCRIDMG.value) + eval(GB.value)) * 0.01) * 1 +
+                eval(SKILLLV.value))) *
+        (1000 / 1140);
+    YCairosDMG.innerHTML = Math.ceil(Cairos);
+    MAXYCairosDMG.innerHTML = Math.ceil(Cairos * 1.03);
+    MINYCairosDMG.innerHTML = Math.ceil(Cairos * 0.97);
+    MAXallYCairosDMG.innerHTML = Math.ceil(Cairos * 3 * 1.03);
+    MINallYCairosDMG.innerHTML = Math.ceil(Cairos * 3 * 0.97);
+
     //------------------------------------------------------------------------------------------------------------------------------------//
     //카이로스던전 치명타X
     var NCairos;
@@ -127,7 +160,7 @@ function cal() {
             ((eval(AS.value) +
                     eval(WS.value) +
                     eval(LATK.value) +
-                    (eval(FIGHTRUNE.value) * 8) +
+                    eval(FIGHTRUNE.value) * 8 +
                     eval(GB.value)) *
                 0.01)) *
         // INCREASE ATK BUFF
@@ -135,10 +168,15 @@ function cal() {
         0.68 *
         // PLUS CIRDMG
         (1 + eval(SKILLLV.value)) *
-        0.877;
-    NCairosDMG.innerHTML = Math.ceil(NCairos);
+        (1000 / 1140);
 
+    NCairosDMG.innerHTML = Math.ceil(NCairos);
+    MAXNCairosDMG.innerHTML = Math.ceil(NCairos * 1.03);
+    MINNCairosDMG.innerHTML = Math.ceil(NCairos * 0.97);
+    MAXallNCairosDMG.innerHTML = Math.ceil(NCairos * 3 * 1.03);
+    MINallNCairosDMG.innerHTML = Math.ceil(NCairos * 3 * 0.97);
     //------------------------------------------------------------------------------------------------------------------------------------//
+    //길드배틀(기댓값)
     var GuildBattle;
     GuildBattle =
         // BASE ATK + RUNE ATK
@@ -149,7 +187,7 @@ function cal() {
             ((eval(AS.value) +
                     eval(WS.value) +
                     eval(LATK.value) +
-                    (eval(FIGHTRUNE.value) * 8) +
+                    eval(FIGHTRUNE.value) * 8 +
                     eval(FB.value)) *
                 0.01)) *
         // INCREASE ATK BUFF
@@ -157,15 +195,50 @@ function cal() {
         0.68 *
         // PLUS CIRDMG
         (1 +
-            ((eval(CRI.value) +
-                    eval(FAG.value) +
-                    eval(LCRIDMG.value) +
-                    eval(FR.value)) *
-                0.01 +
+            (((eval(CRI.value) + eval(FAG.value) + eval(LCRIDMG.value) + eval(FR.value)) * 0.01) * eval(CRIRATE.value) * 0.01 +
                 eval(SKILLLV.value))) *
-        0.877;
+        (1000 / 1140);
+
+
     GuildBattleDMG.innerHTML = Math.ceil(GuildBattle);
+    MAXGuildBattleDMG.innerHTML = Math.ceil(GuildBattle * 1.03);
+    MINGuildBattleDMG.innerHTML = Math.ceil(GuildBattle * 0.97);
+    MAXallGuildBattleDMG.innerHTML = Math.ceil(GuildBattle * 3 * 1.03);
+    MINallGuildBattleDMG.innerHTML = Math.ceil(GuildBattle * 3 * 0.97);
+
     //------------------------------------------------------------------------------------------------------------------------------------//
+    //길드배틀 전부치명타
+    var GuildBattle;
+    GuildBattle =
+        // BASE ATK + RUNE ATK
+        (900 +
+            eval(ATK.value) +
+            // ARENA and READER ATK
+            900 *
+            ((eval(AS.value) +
+                    eval(WS.value) +
+                    eval(LATK.value) +
+                    eval(FIGHTRUNE.value) * 8 +
+                    eval(FB.value)) *
+                0.01)) *
+        // INCREASE ATK BUFF
+        eval(IAP.value) *
+        0.68 *
+        // PLUS CIRDMG
+        (1 +
+            (((eval(CRI.value) + eval(FAG.value) + eval(LCRIDMG.value) + eval(FR.value)) * 0.01) * 1 +
+                eval(SKILLLV.value))) *
+        (1000 / 1140);
+
+
+    YGuildBattleDMG.innerHTML = Math.ceil(GuildBattle);
+    MAXYGuildBattleDMG.innerHTML = Math.ceil(GuildBattle * 1.03);
+    MINYGuildBattleDMG.innerHTML = Math.ceil(GuildBattle * 0.97);
+    MAXallYGuildBattleDMG.innerHTML = Math.ceil(GuildBattle * 3 * 1.03);
+    MINallYGuildBattleDMG.innerHTML = Math.ceil(GuildBattle * 3 * 0.97);
+
+    //------------------------------------------------------------------------------------------------------------------------------------//
+    //길드배틀 치명타X
     var NGuildBattle;
     NGuildBattle =
         // BASE ATK + RUNE ATK
@@ -176,7 +249,7 @@ function cal() {
             ((eval(AS.value) +
                     eval(WS.value) +
                     eval(LATK.value) +
-                    (eval(FIGHTRUNE.value) * 8) +
+                    eval(FIGHTRUNE.value) * 8 +
                     eval(FB.value)) *
                 0.01)) *
         // INCREASE ATK BUFF
@@ -184,11 +257,15 @@ function cal() {
         0.68 *
         // PLUS CIRDMG
         (1 + eval(SKILLLV.value)) *
-        0.877;
+        (1000 / 1140);
     NGuildBattleDMG.innerHTML = Math.ceil(NGuildBattle);
+    MAXNGuildBattleDMG.innerHTML = Math.ceil(NGuildBattle * 1.03);
+    MINNGuildBattleDMG.innerHTML = Math.ceil(NGuildBattle * 0.97);
+    MAXallNGuildBattleDMG.innerHTML = Math.ceil(NGuildBattle * 3 * 1.03);
+    MINallNGuildBattleDMG.innerHTML = Math.ceil(NGuildBattle * 3 * 0.97);
 
     //------------------------------------------------------------------------------------------------------------------------------------//
-    // 아레나
+    // 아레나(기댓값)
     var ARENA;
     ARENA =
         // BASE ATK + RUNE ATK
@@ -199,18 +276,60 @@ function cal() {
             ((eval(AS.value) +
                     eval(WS.value) +
                     eval(LATK.value) +
-                    (eval(FIGHTRUNE.value) * 8)) *
+                    eval(FIGHTRUNE.value) * 8) *
                 0.01)) *
         // INCREASE ATK BUFF
         eval(IAP.value) *
         0.68 *
         // PLUS CIRDMG
+
         (1 +
-            ((eval(CRI.value) + eval(FAG.value)) * 0.01 +
+            (((eval(CRI.value) + eval(FAG.value) + eval(LCRIDMG.value)) * 0.01) * eval(CRIRATE.value) * 0.01 +
                 eval(SKILLLV.value))) *
-        0.877;
+        (1000 / 1140);
+
+
     ArenaDMG.innerHTML = Math.ceil(ARENA);
+    MAXArenaDMG.innerHTML = Math.ceil(ARENA * 1.03);
+    MINArenaDMG.innerHTML = Math.ceil(ARENA * 0.97);
+    MAXallArenaDMG.innerHTML = Math.ceil(ARENA * 3 * 1.03);
+    MINallArenaDMG.innerHTML = Math.ceil(ARENA * 3 * 0.97);
+
+
     //------------------------------------------------------------------------------------------------------------------------------------//
+    // 아레나(기댓값)
+    var ARENA;
+    ARENA =
+        // BASE ATK + RUNE ATK
+        (900 +
+            eval(ATK.value) +
+            // ARENA and READER ATK
+            900 *
+            ((eval(AS.value) +
+                    eval(WS.value) +
+                    eval(LATK.value) +
+                    eval(FIGHTRUNE.value) * 8) *
+                0.01)) *
+        // INCREASE ATK BUFF
+        eval(IAP.value) *
+        0.68 *
+        // PLUS CIRDMG
+
+        (1 +
+            (((eval(CRI.value) + eval(FAG.value) + eval(LCRIDMG.value)) * 0.01) * 1 +
+                eval(SKILLLV.value))) *
+        (1000 / 1140);
+
+
+    YArenaDMG.innerHTML = Math.ceil(ARENA);
+    MAXYArenaDMG.innerHTML = Math.ceil(ARENA * 1.03);
+    MINYArenaDMG.innerHTML = Math.ceil(ARENA * 0.97);
+    MAXallYArenaDMG.innerHTML = Math.ceil(ARENA * 3 * 1.03);
+    MINallYArenaDMG.innerHTML = Math.ceil(ARENA * 3 * 0.97);
+
+
+    //------------------------------------------------------------------------------------------------------------------------------------//
+    //아레나 치명타X
     var NARENA;
     NARENA =
         // BASE ATK + RUNE ATK
@@ -221,22 +340,22 @@ function cal() {
             ((eval(AS.value) +
                     eval(WS.value) +
                     eval(LATK.value) +
-                    (eval(FIGHTRUNE.value) * 8) +
-                    eval(GB.value)) *
+                    eval(FIGHTRUNE.value) * 8) *
                 0.01)) *
         // INCREASE ATK BUFF
         eval(IAP.value) *
         0.68 *
         // PLUS CIRDMG
         (1 + eval(SKILLLV.value)) *
-        0.877;
+        (1000 / 1140);
     NArenaDMG.innerHTML = Math.ceil(NARENA);
+    MAXNArenaDMG.innerHTML = Math.ceil(NARENA * 1.03);
+    MINNArenaDMG.innerHTML = Math.ceil(NARENA * 0.97);
+    MAXallNArenaDMG.innerHTML = Math.ceil(NARENA * 3 * 1.03);
+    MINallNArenaDMG.innerHTML = Math.ceil(NARENA * 3 * 0.97);
 
     //------------------------------------------------------------------------------------------------------------------------------------//
-    allCairosDMG.innerHTML = Math.ceil(Cairos) * 3;
-    allNCairosDMG.innerHTML = Math.ceil(NCairos) * 3;
-    allGuildBattleDMG.innerHTML = Math.ceil(GuildBattle) * 3;
-    allNGuildBattleDMG.innerHTML = Math.ceil(NGuildBattle) * 3;
-    allArenaDMG.innerHTML = Math.ceil(ARENA) * 3;
-    allNArenaDMG.innerHTML = Math.ceil(NARENA) * 3;
+
+
+
 }
